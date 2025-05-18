82c9a1d78d963847e83f4ad587cc0241a6ee439c
Wendy Rizo <wendymartinezrizo@gmail.com>
2025-05-17 20:25:34 -0600

    first commit

---
+++ src/data/projects.ts
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    title: "Title",
+    image: "/project-sample.webp",
+    decription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed diam dolor. Nulla hendrerit massa non aliquam interdum. Vestibulum maximus pretium urna ac volutpat. ",
+    repo: "https://github.com/wendyrizo",
+    preview: "#",
+    technologies: ["Item"],
+  },
+  {
+    title: "Title",
+    image: "/project-sample.webp",
+    decription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed diam dolor. Nulla hendrerit massa non aliquam interdum. Vestibulum maximus pretium urna ac volutpat. ",
+    repo: "https://github.com/wendyrizo",
+    preview: "#",
+    technologies: ["Item"],
+  },
+];
